e81cf01ded1f32815f0bbe6187908b967d929799
luobotang <tangkangxing@sina.com>
2015-11-05 09:28:18 +0800

    refactor

--- read.js
+++ read.js
@@ -2,14 +2,12 @@
 
 	tryInit();
 
+	// init as soon as possible
+	// even before document ready
 	function tryInit() {
-		try {
-			if (document.body) {
-				init();
-			} else {
-				throw new Error('document not ready');
-			}
-		} catch (e) {
+		if (document.body) {
+			init();
+		} else {
 			setTimeout(tryInit, 100);
 		}
 	}
@@ -48,37 +46,109 @@
 	}
 
 	function getContent() {
-		var site = location.hostname;
-		if (site.indexOf('gamersky') > -1) {
-			return $('.Mid2L_con').html();
-		} else if (site.indexOf('movie.douban') > -1) {
+		var content = ContentGenerator.generatorContent();
+		if (content) {
 			return (
-				'<h1>' + $('h1').text() + '</h1>' +
-				$('#link-report').html()
-			);
-		} else if (site.indexOf('jianshu') > -1) {
-			return (
-				'<h1>' + $('h1.title').text() + '</h1>' +
-				$('.show-content').html()
-			);
-		} else if (site.indexOf('baike.baidu') > -1) {
-			return (
-				'<h1>' + $('h1').text() + '</h1>' +
-				$('.main-content').html()
-			);
-		} else if (site.indexOf('tieba.baidu') > -1) {
-			return (
-				'<h1>' + $('h1').text() + '</h1>' +
-				[].map.call($('.d_post_content'), function (content) {
-					return (
-					'<section>' + 
-						content.innerHTML +
-					'</section>'
-					);
-				}).join('')
+				'<h1>' + content.title + '</h1>' +
+				content.content
 			);
 		} else {
 			return null;
+		}
+	}
+
+	var ContentGenerator = {};
+
+	ContentGenerator.generators = [];
+
+	/**
+	 * @param {Object} siteGenerator
+	 * - name {sring}
+	 * - match {function}
+	 * - title {function}
+	 * - content {function}
+	 */
+	ContentGenerator.add = function (siteGenerator) {
+		if (siteGenerator && siteGenerator.match) {
+			this.generators.push(siteGenerator);
+		}
+		return this;
+	};
+
+	ContentGenerator.generatorContent = function () {
+		var siteGenerator;
+		var hostname = location.hostname;
+		for (var i = 0, len = this.generators.length; i < len; i++) {
+			siteGenerator = this.generators[i];
+			if (siteGenerator.match(hostname)) {
+				return {
+					title: siteGenerator.title(),
+					content: siteGenerator.content()
+				};
+			}
+		}
+		return null;
+	}
+
+	// all site generators
+	ContentGenerator
+	.add({
+		name: 'gamersky.com',
+		match: hasName,
+		title: getH1Text,
+		content: getHtml('.Mid2L_con')
+	})
+	.add({
+		name: 'movie.douban',
+		match: hasName,
+		title: getH1Text,
+		content: getHtml('#link-report')
+	})
+	.add({
+		name: 'baike.baidu',
+		match: hasName,
+		title: getH1Text,
+		content: getHtml('.main-content')
+	})
+	.add({
+		name: 'jianshu',
+		match: hasName,
+		title: getText('h1.title'),
+		content: getHtml('.show-content')
+	})
+	.add({
+		name: 'tieba.baidu',
+		match: hasName,
+		title: getH1Text,
+		content: function () {
+			return [].map.call(
+			$('.d_post_content'), function (content) {
+				return (
+				'<section>' + 
+					content.innerHTML +
+				'</section>'
+				);
+			}).join('');
+		}
+	})
+
+	function hasName(hostname) {
+		return hostname.indexOf(this.name) > -1;
+	}
+
+	function getH1Text() {
+		return $('h1').text();
+	}
+
+	function getText(el) {
+		return function () {
+			return $(el).text();
+		}
+	}
+
+	function getHtml(el) {
+		return function () {
+			return $(el).html();
 		}
 	}
 })()
